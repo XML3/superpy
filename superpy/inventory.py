e271e0e4c762ad26a401ba3c2e84349961cf5842
Xagly Montilva <xaglymontilva@gmail.com>
2024-02-12 17:39:40 +0100

    added a function to load data and be called in other functions, added logic for purchase_price, sale_price, expiry_date, and expiry_status

--- superpy/inventory.py
+++ superpy/inventory.py
@@ -2,10 +2,21 @@ import csv
 from today import *
 from create_id import*
 
-#User will also be able to searchh inventory by date 
+#User will also be able to search inventory by date 
+
+#Load inventory data to be used across different function calls
+def load_inventory():
+    inventory = []
+    with open('inventory.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: 
+            inventory.append(row)
+    return inventory
+
 
 def current_inventory():
     current_date = get_current_date()
+    print_inventory_data()
     
     #display inventory
 def inventory_header():
@@ -19,12 +30,12 @@ def inventory_header():
     #write header if new or empty
     if is_new_file:
         with open('inventory.csv', 'w', newline='') as csvfile:
-            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock', 'update_date']
+            fieldnames = ['product_id', 'product', 'price', 'purchase_price', 'quantity_bought', 'quantity_sold', 'in_stock', 'expiry_date', 'sale_price', 'expiry_status', 'update_date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             
 #Prep Data for CSV 
-def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
+def add_inventory(product, price, purchase_price, quantity_bought, quantity_sold, in_stock, expiry_date, sale_price, expiry_status, update_date):
     try:
         #generate unique ID for each product
         product_id = generate_product_id()
@@ -34,9 +45,13 @@ def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
             'product_id': product_id,
             'product': product,
             'price': price,
+            'purchase_price': purchase_price,
             'quantity_bought': quantity_bought,
             'quantity_sold': quantity_sold,
             'in_stock': in_stock,
+            'expiry_date': expiry_date,
+            'sale_price': sale_price,
+            'expiry_status': expiry_status,
             #User can search invetory by date
             'update_date': update_date
         }
@@ -45,13 +60,41 @@ def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
         
         #append new inventory to the file
         with open('inventory.csv', 'a', newline='') as csvfile:
-            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock', 'update_date']
+            fieldnames = ['product_id', 'product', 'price', 'purchase_price', 'quantity_bought', 'quantity_sold', 'in_stock', 'expiry_date', 'sale_price', 'expiry_status', 'update_date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(inventory_data)
             
     except Exception as e:
         print(f"An error occurred while adding items to inventory: {e}")
         
+#Get purchase price for product and call inventory function to read/load data
+def get_purchase_price(product, inventory):
+    for row in inventory:
+        if row['product'] == product:
+            return row['price']
+    return None
+
+#Get expiry_date call function
+def get_expiry_date(product, inventory):
+      for row in inventory:
+          if row['product'] == product:
+              return row['expiry_date']
+      return None
+  
+  #Get sale_price call function      
+def get_sale_price(product, inventory):
+    for row in inventory:
+        if row['product'] == product:
+            return row['sale_price']
+    return None
+    
+#Get expirary_status, call function
+def get_expiry_status(product, inventory):
+    for row in inventory:
+        if row['product'] == product:
+            return row['expiry_status']
+    return None
+
 #Read and Print content of inventory file
 def print_inventory_data(date=None):
     with open('inventory.csv', 'r') as csvfile:
