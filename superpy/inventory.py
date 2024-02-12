f2658d15c1fddffd2455fd0ddf1d26863208193b
Xagly Montilva <xaglymontilva@gmail.com>
2024-02-12 13:24:43 +0100

    added functionality to the code for user to search inventoty by date

--- superpy/inventory.py
+++ superpy/inventory.py
@@ -2,6 +2,7 @@ import csv
 from today import *
 from create_id import*
 
+#User will also be able to searchh inventory by date 
 
 def current_inventory():
     current_date = get_current_date()
@@ -18,7 +19,7 @@ def inventory_header():
     #write header if new or empty
     if is_new_file:
         with open('inventory.csv', 'w', newline='') as csvfile:
-            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock']
+            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock', 'update_date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             
@@ -27,6 +28,7 @@ def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
     try:
         #generate unique ID for each product
         product_id = generate_product_id()
+        update_date = get_current_date()
         
         inventory_data = {
             'product_id': product_id,
@@ -34,14 +36,16 @@ def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
             'price': price,
             'quantity_bought': quantity_bought,
             'quantity_sold': quantity_sold,
-            'in_stock': in_stock
+            'in_stock': in_stock,
+            #User can search invetory by date
+            'update_date': update_date
         }
         #write header if needed
         inventory_header()
         
         #append new inventory to the file
         with open('inventory.csv', 'a', newline='') as csvfile:
-            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock']
+            fieldnames = ['product_id', 'product', 'price', 'quantity_bought', 'quantity_sold', 'in_stock', 'update_date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(inventory_data)
             
@@ -49,13 +53,15 @@ def add_inventory(product, price, quantity_bought, quantity_sold, in_stock):
         print(f"An error occurred while adding items to inventory: {e}")
         
 #Read and Print content of inventory file
-def print_inventory_data():
+def print_inventory_data(date=None):
     with open('inventory.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
         print(header)
+        
         for row in reader:
-            print(row)
+            if date is None or row['update_date'] == date:
+                print(row)
             
 if __name__ == "__main__":
     add_inventory('Apple', 1.30, 5, 2, 3)
