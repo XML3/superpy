01418cced2875a09004ab1ef5f9e46f889e04ef8
Xagly Montilva <xaglymontilva@gmail.com>
2024-02-14 13:04:31 +0100

    created Class for Rich Text to be used across the app

---
+++ superpy/rich_utils.py
@@ -0,0 +1,14 @@
+from rich.console import Console
+
+class RichUtils:
+    def __inti__(self):
+        self.console = Console()
+        
+    def print_styled_text(self, text, style=""):
+        self.console.print(text, style=style)
+        
+    def print_table(self, header, rows):
+        self.console.print(header)
+        for row in rows:
+            self.console.print(row)
+            
