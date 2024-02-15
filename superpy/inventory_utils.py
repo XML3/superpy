5e4d675a6642889820ac01829e8f6dee1a4c6130
Xagly Montilva <xaglymontilva@gmail.com>
2024-02-15 19:17:34 +0100

    added function to load sold data to be used in the expire_dates file

--- superpy/inventory_utils.py
+++ superpy/inventory_utils.py
@@ -9,3 +9,12 @@ def load_inventory():
         for row in reader: 
             inventory.append(row)
     return inventory
+
+#Load sold data to be used across app.
+def load_sold_data():
+    sold = []
+    with open('sold.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            sold.append(row)
+    return sold
