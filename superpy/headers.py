d537a00a30f5a282a93a8e3cb3861208ffb9ffbd
Xagly Montilva <xaglymontilva@gmail.com>
2024-02-18 13:31:57 +0100

    nothing

--- superpy/headers.py
+++ superpy/headers.py
@@ -1,8 +1,9 @@
-
+from enum import Enum
 
 #header for CSV files / Richtables
-bought_header = ['purchase_id', 'product', 'price', 'quantity', 'expiration_date', 'purchase_date']
-sold_header = ['sold_id', 'bought_id', 'product', 'sell_price', 'quantity', 'sell_date']
-inventory_header = ['product_id', 'product', 'price', 'purchase_price', 'quanty_bought', 'quantity_sold', 'in_stock', 'expiry_date', 'sale_price', 'expiry_status', 'update_date']
-expired_header = ['product_id', 'product', 'expiry_date', 'expired_sold']
-revenue_header = ['period', 'revenue', 'cost']
+class HeaderType(Enum):
+    BBOUGHT= ['purchase_id', 'product', 'price', 'quantity', 'expiration_date', 'purchase_date']
+    SOLD = ['sold_id', 'bought_id', 'product', 'sell_price', 'quantity', 'sell_date']
+    INVENTORY = ['product_id', 'product', 'price', 'purchase_price', 'quanty_bought', 'quantity_sold', 'in_stock', 'expiry_date', 'sale_price', 'expiry_status', 'update_date']
+    EXPIRED = ['product_id', 'product', 'expiry_date', 'expired_sold']
+    REVENUE = ['period', 'revenue', 'cost']
